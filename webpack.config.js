a0c3d827e619bd539d9b6c04dbf6710e0e850784
thedocbwarren <jedi.master@augmentedjs.com>
2020-09-01 12:20:04 -0700

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Package = require("./package.json");
+const isProd = process.argv[process.argv.indexOf("--mode") + 1] === "production";
+
+console.info(`Mode: ${((isProd) ? "Production" : "Development")}`);
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   context: __dirname,
   target: "web",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'presentation-router.js',
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "presentation-router.js",
+    publicPath: "/dist/",
     library: "presentation-router",
-    globalObject: 'this',
+    globalObject: "this",
     libraryTarget: "umd",
     umdNamedDefine: true
   },
@@ -26,42 +31,45 @@ module.exports = {
     ]
   },
   externals: {
-    'next-core-object': {
-      commonjs: 'next-core-object',
-      commonjs2: 'next-core-object',
-      amd: 'next-core-object',
-      root: 'next-core-object'
+    "next-core-object": {
+      commonjs: "next-core-object",
+      commonjs2: "next-core-object",
+      amd: "next-core-object",
+      root: "next-core-object"
     },
-    'next-core-utilities': {
-      commonjs: 'next-core-utilities',
-      commonjs2: 'next-core-utilities',
-      amd: 'next-core-utilities',
-      root: 'next-core-utilities'
+    "next-core-utilities": {
+      commonjs: "next-core-utilities",
+      commonjs2: "next-core-utilities",
+      amd: "next-core-utilities",
+      root: "next-core-utilities"
     },
-    'presentation-dom': {
-      commonjs: 'presentation-dom',
-      commonjs2: 'presentation-dom',
-      amd: 'presentation-dom',
-      root: 'presentation-dom'
+    "presentation-dom": {
+      commonjs: "presentation-dom",
+      commonjs2: "presentation-dom",
+      amd: "presentation-dom",
+      root: "presentation-dom"
     },
-    'lodash.map': {
-      commonjs: 'lodash.map',
-      commonjs2: 'lodash.map',
-      amd: 'lodash.map',
-      root: 'lodash.map'
+    "lodash.map": {
+      commonjs: "lodash.map",
+      commonjs2: "lodash.map",
+      amd: "lodash.map",
+      root: "lodash.map"
     },
-    'lodash.isregexp': {
-      commonjs: 'lodash.isregexp',
-      commonjs2: 'lodash.isregexp',
-      amd: 'lodash.isregexp',
-      root: 'lodash.isregexp'
+    "lodash.isregexp": {
+      commonjs: "lodash.isregexp",
+      commonjs2: "lodash.isregexp",
+      amd: "lodash.isregexp",
+      root: "lodash.isregexp"
     }
   },
   stats: "errors-only",
-  devtool: "source-map",
+  devtool: (isProd) ? "none": "source-map",
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      VERSION: JSON.stringify(require("./package.json").version)
+      VERSION: JSON.stringify(Package.version),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      "process.env.DEBUG": JSON.stringify(process.env.DEBUG)
     })
   ]
 };
